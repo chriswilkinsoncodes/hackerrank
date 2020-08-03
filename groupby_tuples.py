3d99d8d186761c8fe45c1181d5e441f41378bd1d
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-08-03 09:30:05 -0400

    alt version

--- groupby_tuples.py
+++ groupby_tuples.py
@@ -5,4 +5,5 @@ from itertools import groupby
 
 s = input()
 for k, g in groupby(s):
-    print((sum(1 for _ in g), int(k)), end=' ')
+    # print((sum(1 for _ in g), int(k)), end=' ')
+    print((len(list(g)), int(k)), end=' ')
