2d49a400c191f77745fffb690a082f702f5eda4e
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-09-18 22:17:55 -0400

    initial commit

---
+++ integers_come_in_all_sizes.py
@@ -0,0 +1,7 @@
+#/usr/bin/env python3
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# a, b, c, d = (int(input()) for _ in range(4))
+#print(a**b + c**d)
+
+print(int(input())**int(input()) + int(input())**int(input()))
